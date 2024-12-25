b42c28be1f879185909aaa2ff0d8579c892338ab
Aaron Todd <aarontodd@Aarons-MacBook-Pro.local>
2024-12-24 21:09:45 -0500

    Adds LaunchDarkly

--- components/TabContext.js
+++ components/TabContext.js
@@ -1,8 +1,30 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import { useBoolVariation, useLDClient } from '@launchdarkly/react-native-client-sdk'
 
-const TabContext = createContext();
+
+const TabContext = createContext()
 
 export const TabProvider = ({ children }) => {
+  const settingsFlag = useBoolVariation('settings', false)
+  const ldc = useLDClient()
+
+  // FUTURE FEATURE WHEN WE HAVE USER ACCOUNTS
+  // useEffect(() => {
+  //   ldc
+  //     .identify({ kind: 'user', key: 'example-user-key', name: 'Sandy' })
+  //     .catch((e) => console.error('error: ' + e))
+  // }, [])
+
+  useEffect(() => {
+    setTabOrder(tabOrder.map(t => {
+
+      return {
+        ...t,
+        visible: t.name === 'settings' ? settingsFlag : t.visible
+      }
+    }))
+  }, [settingsFlag])
+
   const [tabOrder, setTabOrder] = useState([
     { name: 'index', title: 'Home', visible: true },
     { name: 'calendar', title: 'Calendar', visible: true },
@@ -14,25 +36,25 @@ export const TabProvider = ({ children }) => {
     // { name: 'AppPreferencesSettings', title: 'App Preferences', visible: false },
     // { name: 'AccountSettings', title: 'Account Settings', visible: false },
     // { name: 'NotificationsSettings', title: 'Notifications', visible: false },
-  ]);
+  ])
 
   const toggleTabVisibility = (name) => {
     setTabOrder(prevOrder =>
       prevOrder.map(tab =>
         tab.name === name ? { ...tab, visible: !tab.visible } : tab
       )
-    );
-  };
+    )
+  }
 
   const updateTabOrder = (newOrder) => {
-    setTabOrder(newOrder);
-  };
+    setTabOrder(newOrder)
+  }
 
   return (
     <TabContext.Provider value={{ tabOrder, toggleTabVisibility, updateTabOrder }}>
       {children}
     </TabContext.Provider>
-  );
-};
+  )
+}
 
-export const useTabContext = () => useContext(TabContext);
+export const useTabContext = () => useContext(TabContext)
