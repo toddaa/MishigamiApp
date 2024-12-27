696280598ef440f7d08bdb1c72cc7ccab066940b
Aaron Todd <toddaa@gmail.com>
2024-12-27 11:22:25 -0500

    cleanup

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StyleSheet, Image, Platform } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
@@ -24,37 +24,31 @@ const dateTimeOptions = {
   // hour12:true,
   // timeZoneName: 'short'
 }
-export default function TabTwoScreen() {
-  const [pageToken, setPageToken] =useState('')
-  const [events, setEvents] =useState([])
-  const [viewableEvents, setViewableEvents] =useState([])
+export default function TabTwoScreen () {
+  const [pageToken, setPageToken] = useState('')
+  const [events, setEvents] = useState([])
+  const [viewableEvents, setViewableEvents] = useState([])
 
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState([]);
   const [items, setItems] = useState([
-    {label: 'Lodge Executive Committee', value: 'Lodge Executive Committee'},
-    {label: 'Inductions', value: 'Inductions' },
-    {label: 'Lodge Events', value: 'Lodge Events' },
+    { label: 'Lodge Executive Committee', value: 'Lodge Executive Committee' },
+    { label: 'Inductions', value: 'Inductions' },
+    { label: 'Lodge Events', value: 'Lodge Events' },
 
   ]);
 
   useEffect(() => {
-    async function fetchData() {
+    async function fetchData () {
       const now = new Date();
       const startOfYearDate = startOfYear(now);
       let endOfYearDate = endOfYear(now);
 
-      console.log(differenceInDays(new Date(startOfYearDate), new Date(endOfYearDate)))
-
-      if (differenceInDays(new Date(startOfYearDate), new Date(endOfYearDate)) < 60){
-        endOfYearDate = add(new Date(endOfYearDate), {days: 60})
+      if (differenceInDays(new Date(startOfYearDate), new Date(endOfYearDate)) < 60) {
+        endOfYearDate = add(new Date(endOfYearDate), { days: 60 })
       }
 
-      console.log({startOfYearDate})
-      console.log({endOfYearDate})
-
-      const events = await client.graphql({ query: listEvents, variables: {filter: {startDate: {ge: startOfYearDate}, endDate: {lt: endOfYearDate}}} });
-      // console.log(events.data.listEvents.items)
+      const events = await client.graphql({ query: listEvents, variables: { filter: { startDate: { ge: startOfYearDate }, endDate: { lt: endOfYearDate } } } });
       setEvents(events.data.listEvents.items)
     }
     fetchData();
@@ -64,7 +58,7 @@ export default function TabTwoScreen() {
   useEffect(() => {
   }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     setViewableEvents(events.sort(sortStartDesc))
   }, [events]);
 
@@ -81,17 +75,15 @@ export default function TabTwoScreen() {
   }
 
   const filterChange = (value) => {
-    // console.log(value);
     let temp = []
-    if (value.length<1){
-      setViewableEvents (events)
+    if (value.length < 1) {
+      setViewableEvents(events)
     } else {
       temp = events.map((e) => {
         if (value.includes(e.summary)) {
           return e
         }
       }).filter(val => val !== undefined)
-      // console.log ({temp})
       setViewableEvents(temp)
     }
 
@@ -100,13 +92,13 @@ export default function TabTwoScreen() {
 
   return (
     <ParallaxScrollView
-    headerBackgroundColor={{ light: '#A1CEDC', dark: '#799FAF' }}
-    headerImage={
-      <Image
-        source={require('@/assets/images/Mishigami-Blue-Mastodon-Arrow-Text.png')}
-        style={styles.reactLogo}
-      />
-    }>
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#799FAF' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/Mishigami-Blue-Mastodon-Arrow-Text.png')}
+          style={styles.reactLogo}
+        />
+      }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Calendar</ThemedText>
       </ThemedView>
@@ -127,40 +119,40 @@ export default function TabTwoScreen() {
       />
 
       {
-        viewableEvents.map((e,i) => {
-          let endDate =''
+        viewableEvents.map((e, i) => {
+          let endDate = ''
           if (e?.endDate !== undefined) {
             endDate = new Intl.DateTimeFormat('en-US', dateTimeOptions).format(new Date(e?.endDate))
           }
 
-          let startDate =''
+          let startDate = ''
           if (e?.startDate !== undefined) {
             startDate = new Intl.DateTimeFormat('en-US', dateTimeOptions).format(new Date(e?.startDate))
           }
 
-          let desc =''
+          let desc = ''
           if (e?.description !== undefined) {
             desc = e?.description
           }
           let title = e?.name
-          if (startDate !=='' ){
-            title =`${title} ${startDate}`
+          if (startDate !== '') {
+            title = `${title} ${startDate}`
           }
-          if (endDate !=='' ){
-            title =`${title} to ${endDate}`
+          if (endDate !== '') {
+            title = `${title} to ${endDate}`
           }
           return <Collapsible key={i} title={title}>
-          <ThemedText>
-            {desc}
-          </ThemedText>
-          <ThemedText>
-            Location: {e?.location}
-          </ThemedText>
+            <ThemedText>
+              {desc}
+            </ThemedText>
+            <ThemedText>
+              Location: {e?.location}
+            </ThemedText>
 
-          <ExternalLink href="https://docs.expo.dev/router/introduction">
-            <ThemedText type="link">Learn more</ThemedText>
-          </ExternalLink>
-        </Collapsible>
+            <ExternalLink href="https://docs.expo.dev/router/introduction">
+              <ThemedText type="link">Learn more</ThemedText>
+            </ExternalLink>
+          </Collapsible>
         })
       }
     </ParallaxScrollView>
